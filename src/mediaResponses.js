391dfe0d6062cc1b7677cda80cdfd9e30e5a4485
Nicholas Zonenberg <nicholaszonenberg@gmail.com>
2018-09-11 00:30:44 -0400

    Finished

---
+++ src/mediaResponses.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+
+function loadFile(requestVar, responseVar, filePath, contentType) {
+  const file = path.resolve(__dirname, filePath);
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        responseVar.writeHead(404);
+      }
+      return responseVar.end(err);
+    }
+
+    let { range } = requestVar.headers;
+
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    const positions = range.replace(/bytes=/, '').split('-');
+
+    let start = parseInt(positions[0], 10);
+
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+    if (start > end) {
+      start = end - 1;
+    }
+
+    const chunksize = (end - start) + 1;
+
+    responseVar.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': contentType,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(responseVar);
+    });
+
+    stream.on('error', (streamErr) => {
+      responseVar.edn(streamErr);
+    });
+
+    return stream;
+  });
+}
+
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
+module.exports.getParty = getParty;
+module.exports.getBird = getBird;
+module.exports.getBling = getBling;
